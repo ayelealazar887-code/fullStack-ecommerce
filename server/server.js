f6b599de45fdfbe3de04ec324273a0b6662743fa
ayelealazar887-code <ayelealazar887@gmail.com>
2026-08-07 13:56:02 +0300

    authentication completed

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ import mongoose from "mongoose";
 import cors from "cors";
 import authRouter from "./routes/authRouter.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 import dns from 'dns';
+import userRouter from "./routes/userRoute.js";
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 dotenv.config();
@@ -13,14 +15,15 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173/",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Route
 app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
 
 
 connectDB();
